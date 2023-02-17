a7d20b564a3d4724e8cee6158020058e06ff44da
Lucas <lucas.souza3146@gmail.com>
2023-02-17 17:55:43 -0300

    Implementando area de Consulta de lanches por período

---
+++ LanchesMac/Areas/Admin/Servicos/RelatorioVendasService.cs
@@ -0,0 +1,36 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Areas.Admin.Servicos
+{
+    public class RelatorioVendasService
+    {
+        private readonly AppDbContext context;
+
+        public RelatorioVendasService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Pedido>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var resultado = from obj in context.Pedidos select obj;
+
+            if (minDate.HasValue)
+            {
+                resultado = resultado.Where(r => r.PedidoEnviado >= minDate);
+            }
+            if (maxDate.HasValue)
+            {
+                resultado = resultado.Where(r => r.PedidoEnviado <= maxDate);
+            }
+
+            return await resultado
+                            .Include(l => l.PedidoItens)
+                            .ThenInclude(l => l.Lanche)
+                            .OrderByDescending(x => x.PedidoEnviado)
+                            .ToListAsync();
+        }
+    }
+}
--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Context;
+﻿using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Context;
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
@@ -42,6 +43,7 @@ public class Startup
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddScoped<IseedUserRoleInitial, SeedUserRoleInitial>();
+        services.AddScoped<RelatorioVendasService>();
 
         services.AddPaging(options =>
         {
